b82f59ac2ebec319700b90596a2e90a9201a2b3c
ImPurpl3 <cjhilderman@gmail.com>
2025-01-11 17:29:14 -0600

    added web handler

---
+++ quoteHandler.py
@@ -0,0 +1,22 @@
+import rmquote
+from flask import Flask, send_file
+
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    storage_uri="memcached://localhost:11211",
+    default_limits=["200 per day", "50 per hour"],
+)
+
+@app.route("/quote/Rk3Uyk0GVcQZVsBC5iI0kJzSmEY")
+@limiter.limit("100 per day")
+def quote():
+    rmquote.genquote()
+    return send_file("quote.png", mimetype='image/png')
+
+if __name__ == "__main__": 
+    app.run(debug=True)
